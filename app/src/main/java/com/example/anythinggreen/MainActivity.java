47d7e4fbfe44a9389590bd7c214a76b2937d025f
Adrian Lim <72697082+dlmbc@users.noreply.github.com>
2022-12-22 14:55:00 +0800

    Implement FragmentStateAdapter
    - fix fragments

--- app/src/main/java/com/example/anythinggreen/MainActivity.java
+++ app/src/main/java/com/example/anythinggreen/MainActivity.java
@@ -1,51 +1,80 @@
 package com.example.anythinggreen;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
-import com.example.anythinggreen.databinding.ActivityMainBinding;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ViewPager2 viewPager2;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        replaceFragment(new HomeFragment());
+        setContentView(R.layout.activity_main);
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        viewPager2 = findViewById(R.id.view_pager2);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-            if (item.getItemId() == R.id.home) {
-                replaceFragment(new HomeFragment());
+        // Set up the ViewPager2 with a FragmentStateAdapter
+        viewPager2.setAdapter(new MyFragmentStateAdapter(this));
+
+        // Set up the BottomNavigationView with a OnNavigationItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        viewPager2.setCurrentItem(0);
+                        return true;
+                    case R.id.information:
+                        viewPager2.setCurrentItem(1);
+                        return true;
+                    case R.id.rewards:
+                        viewPager2.setCurrentItem(2);
+                        return true;
+                    case R.id.settings:
+                        viewPager2.setCurrentItem(3);
+                        return true;
+                    default:
+                        return false;
+                }
             }
-
-            if (item.getItemId() == R.id.information) {
-                replaceFragment(new InfoFragment());
-            }
-
-            if (item.getItemId() == R.id.rewards) {
-                replaceFragment(new RewardsFragment());
-            }
-
-            if (item.getItemId() == R.id.settings) {
-                replaceFragment(new SettingsFragment());
-            }
-
-            return true;
         });
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout,fragment);
-        fragmentTransaction.commit();
+    private static class MyFragmentStateAdapter extends FragmentStateAdapter {
+        MyFragmentStateAdapter(FragmentActivity fragmentActivity){
+            super(fragmentActivity);
+        }
 
+        public Fragment createFragment(int position){
+            switch (position){
+                case 0:
+                    return new HomeFragment();
+                case 1:
+                    return new InfoFragment();
+                case 2:
+                    return new RewardsFragment();
+                case 3:
+                    return new SettingsFragment();
+                default:
+                    throw new IllegalStateException("Invalid position: "+ position);
+            }
+        }
+
+        public int getItemCount(){
+            return 4;
+        }
     }
 }
